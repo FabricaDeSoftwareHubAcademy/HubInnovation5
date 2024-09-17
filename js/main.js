47c64ad204d56f5f50cc8de6779d2c138fd2753d
Asato Texco <arthur2006.teixeira@gmail.com>
2024-09-16 22:05:29 -0300

    First Commit - Front End criado

---
+++ js/main.js
@@ -0,0 +1,146 @@
+// NECESSÁRIO PEGAR DO BACKEND E INSTANCIAR ESSA VARIAVEL
+const dataPalestras = {
+
+    // id: {}
+    "22":{
+
+        srcImagemPalestra:"https://img.freepik.com/fotos-gratis/pessoas-que-participam-de-um-evento-de-alto-protocolo_23-2150951243.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726099200&semt=ais_hybrid",
+        titlePalestra: "A.I. para Vendas A.I. para Vendas A.I. para Vendas A.I. para Vendas",
+        subtitlePalestra: "Como captar clientes",
+        descPalestra: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a dignissim tortor, ac aliquet nisi. Phasellus semper nec eros ut accumsan. Donec sit amet ante sodales, auctor erat nec, efficitur ante. Pellentesque nunc nisi, laoreet ut sodales at, cursus at dolor. Ut et porttitor urna. Aliquam erat volutpat. Maecenas scelerisque lorem purus, quis posuere orci sollicitudin vel. Sed egestas maximus ultricies. Aliquam mattis, eros in vulputate ullamcorper, tellus justo pulvinar neque, vel rutrum libero dui sit amet purus. Curabitur tempor erat dui, tincidunt mollis augue semper luctus. Praesent imperdiet sem nec tellus vehicula vestibulum. Aliquam dapibus vel eros et pretium. Nulla et semper est. Vivamus convallis libero ac metus porta ultrices. Nullam at pulvinar velit, ac tempus leo. Integer pharetra quam vehicula ligula hendrerit, a faucibus nisi consequat. Quisque viverra vehicula leo, vitae rutrum risus cursus in. Donec vitae urna libero. In euismod venenatis diam a ultricies. Duis massa metus, maximus quis faucibus convallis, porttitor sit amet ex. Suspendisse potenti. Cras scelerisque nibh vel auctor blandit. Morbi mauris nulla, facilisis a odio ut, faucibus pulvinar lacus. Sed finibus nibh sit amet massa imperdiet fringilla. Fusce sed luctus turpis. Proin eget justo viverra, condimentum erat at, malesuada neque.",
+        palestrante: {
+            nome:"Prof. Matheus Silva",
+            fotoSrc:"https://cinepop.com.br/wp-content/uploads/2022/07/hunter-cinepop.jpg"
+        } 
+    },
+    "28":{
+
+        srcImagemPalestra:"https://img.freepik.com/fotos-gratis/empresario-corporativo-fazendo-uma-apresentacao-para-um-grande-publico_53876-101865.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726099200&semt=ais_hybrid",
+        titlePalestra: "A.I. para Vendas",
+        subtitlePalestra: "Como captar clientes",
+        descPalestra: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a dignissim tortor, ac aliquet nisi. Phasellus semper nec eros ut accumsan. Donec sit amet ante sodales, auctor erat nec, efficitur ante. Pellentesque nunc nisi, laoreet ut sodales at, cursus at dolor. Ut et porttitor urna. Aliquam erat volutpat. Maecenas scelerisque lorem purus, quis posuere orci sollicitudin vel. Sed egestas maximus ultricies. Aliquam mattis, eros in vulputate ullamcorper, tellus justo pulvinar neque, vel rutrum libero dui sit amet purus. Curabitur tempor erat dui, tincidunt mollis augue semper luctus. Praesent imperdiet sem nec tellus vehicula vestibulum. Aliquam dapibus vel eros et pretium. Nulla et semper est. Vivamus convallis libero ac metus porta ultrices. Nullam at pulvinar velit, ac tempus leo. Integer pharetra quam vehicula ligula hendrerit, a faucibus nisi consequat. Quisque viverra vehicula leo, vitae rutrum risus cursus in. Donec vitae urna libero. In euismod venenatis diam a ultricies. Duis massa metus, maximus quis faucibus convallis, porttitor sit amet ex. Suspendisse potenti. Cras scelerisque nibh vel auctor blandit. Morbi mauris nulla, facilisis a odio ut, faucibus pulvinar lacus. Sed finibus nibh sit amet massa imperdiet fringilla. Fusce sed luctus turpis. Proin eget justo viverra, condimentum erat at, malesuada neque.",
+        palestrante: {
+            nome:"Prof. Junior",
+            fotoSrc:"https://kanto.legiaodosherois.com.br/w760-h398-cfill/wp-content/uploads/2017/04/legiao_BQY1r2XdZNUGqT9svuKWCpPRc8IkOgVhlmb4o6ywAS.jpg.webp"
+        } 
+    },
+    "13":{
+
+        srcImagemPalestra:"../images/32e8b686f190fae3e64a74c8a8df1ac8.jpg",
+        titlePalestra: "A.I. para Vendas",
+        subtitlePalestra: "Como captar clientes",
+        descPalestra: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a dignissim tortor, ac aliquet nisi. Phasellus semper nec eros ut accumsan. Donec sit amet ante sodales, auctor erat nec, efficitur ante. Pellentesque nunc nisi, laoreet ut sodales at, cursus at dolor. Ut et porttitor urna. Aliquam erat volutpat. Maecenas scelerisque lorem purus, quis posuere orci sollicitudin vel. Sed egestas maximus ultricies. Aliquam mattis, eros in vulputate ullamcorper, tellus justo pulvinar neque, vel rutrum libero dui sit amet purus. Curabitur tempor erat dui, tincidunt mollis augue semper luctus. Praesent imperdiet sem nec tellus vehicula vestibulum. Aliquam dapibus vel eros et pretium. Nulla et semper est. Vivamus convallis libero ac metus porta ultrices. Nullam at pulvinar velit, ac tempus leo. Integer pharetra quam vehicula ligula hendrerit, a faucibus nisi consequat. Quisque viverra vehicula leo, vitae rutrum risus cursus in. Donec vitae urna libero. In euismod venenatis diam a ultricies. Duis massa metus, maximus quis faucibus convallis, porttitor sit amet ex. Suspendisse potenti. Cras scelerisque nibh vel auctor blandit. Morbi mauris nulla, facilisis a odio ut, faucibus pulvinar lacus. Sed finibus nibh sit amet massa imperdiet fringilla. Fusce sed luctus turpis. Proin eget justo viverra, condimentum erat at, malesuada neque.",
+        palestrante: {
+            nome:"Prof. Jegue",
+            fotoSrc:"https://www.marliesdekkers.com/dw/image/v2/AARF_PRD/on/demandware.static/-/Library-Sites-Library-Universal/default/dwd7ea7e3b/images/wordpress/uploads/2016/01/Lagertha-Embrace-Your-Inner-Viking-Warrior-1.jpg"
+        } 
+    }, 
+    "11":{
+
+        srcImagemPalestra:"https://media.istockphoto.com/id/499517325/pt/foto/um-homem-falando-em-uma-confer%C3%AAncia-de-neg%C3%B3cios.jpg?s=612x612&w=0&k=20&c=yxUfXB3YGNK9trUiaFJrOJBQxrDzYLfTynw-2OHt-ys=",
+        titlePalestra: "A.I. para Vendas",
+        subtitlePalestra: "Como captar clientes",
+        descPalestra: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a dignissim tortor, ac aliquet nisi. Phasellus semper nec eros ut accumsan. Donec sit amet ante sodales, auctor erat nec, efficitur ante. Pellentesque nunc nisi, laoreet ut sodales at, cursus at dolor. Ut et porttitor urna. Aliquam erat volutpat. Maecenas scelerisque lorem purus, quis posuere orci sollicitudin vel. Sed egestas maximus ultricies. Aliquam mattis, eros in vulputate ullamcorper, tellus justo pulvinar neque, vel rutrum libero dui sit amet purus. Curabitur tempor erat dui, tincidunt mollis augue semper luctus. Praesent imperdiet sem nec tellus vehicula vestibulum. Aliquam dapibus vel eros et pretium. Nulla et semper est. Vivamus convallis libero ac metus porta ultrices. Nullam at pulvinar velit, ac tempus leo. Integer pharetra quam vehicula ligula hendrerit, a faucibus nisi consequat. Quisque viverra vehicula leo, vitae rutrum risus cursus in. Donec vitae urna libero. In euismod venenatis diam a ultricies. Duis massa metus, maximus quis faucibus convallis, porttitor sit amet ex. Suspendisse potenti. Cras scelerisque nibh vel auctor blandit. Morbi mauris nulla, facilisis a odio ut, faucibus pulvinar lacus. Sed finibus nibh sit amet massa imperdiet fringilla. Fusce sed luctus turpis. Proin eget justo viverra, condimentum erat at, malesuada neque.",
+        palestrante: {
+            nome:"Prof. Huan Cabalero",
+            fotoSrc:"../images/32e8b686f190fae3e64a74c8a8df1ac8.jpg"
+        } 
+    },
+
+}
+
+// TEXTOS SOBRE
+// [0] = O QUE É 
+// [1] = PARA QUEM
+// [2] = POR QUE IR
+const textsAbout = [
+    "O Hub Innovation não é apenas um espaço físico; é uma incubadora de ideias e um catalisador para a transformação social e profissional. Localizado no coração da cidade, esse hub se destaca não só pela localização privilegiada, mas também por um ambiente que respira tecnologia e inovação. A estrutura é projetada para facilitar a interação e o compartilhamento de conhecimentos, promovendo a emancipação dos papéis individuais em um contexto que valoriza tanto a autonomia quanto a colaboração. É um local onde futuros líderes são forjados, novas ideias são concebidas e a inovação é uma constante. Cada canto do Hub está equipado para incentivar a criatividade e a inovação, tornando-o um ponto de encontro essencial para qualquer pessoa interessada em moldar o futuro. Investir seu tempo para conhecer o Hub Innovation é uma decisão que promete retornos significativos em crescimento pessoal e profissional.",
+    "O Hub Innovation é um verdadeiro mosaico cultural e intelectual, aberto a pessoas de todas as idades, lugares e backgrounds. Este hub não se limita a ser apenas um espaço de trabalho, mas um ambiente de aprendizado e crescimento onde cada atividade e palestra é meticulosamente planejada para ser acessível e inclusiva. A diversidade é celebrada e encorajada, com eventos que vão desde workshops sobre as mais recentes tecnologias até seminários sobre saúde mental e bem-estar. Participar do Hub Innovation significa ter a liberdade de explorar seus interesses e paixões em um ambiente que apoia e nutre o desenvolvimento individual e coletivo. A filosofia do hub é que a educação e o desenvolvimento devem ser livres e espontâneos, permitindo que cada indivíduo descubra novas paixões e desenvolva habilidades de maneira orgânica e sustentável.",
+    "Ao entrar no Hub Innovation, você imediatamente percebe que está entrando em um território onde o potencial é infinito. A energia do lugar é contagiante, com espaços vibrantes repletos de pessoas conversando, colaborando e criando. Aqui, você não apenas adquire um network robusto e diversificado, mas também se expõe a uma ampla gama de conhecimentos que atravessam disciplinas tradicionais. Desde aprendizados sobre estética e cuidados pessoais até oficinas avançadas sobre inteligência artificial e análise de dados, o Hub proporciona uma educação holística que prepara você para os desafios do mundo moderno. Além disso, ao frequentar o Hub Innovation, você tem a oportunidade de participar de projetos colaborativos, seminários de liderança e sessões de mentoria que são projetadas não apenas para ensinar, mas para transformar. Prepare-se para uma jornada de aprendizado e descoberta que vai além do convencional, expandindo suas perspectivas e aprimorando suas habilidades de maneira que poucos lugares podem oferecer."
+];
+ 
+
+const palestras_cards = document.querySelectorAll(".palestra_item")
+palestras_cards.forEach(e => {  
+
+    const id_palestra = e.getAttribute("palestra_id") 
+    e.addEventListener('click',() => { 
+        abrirModalSubscribe(dataPalestras[id_palestra]) 
+    }) 
+
+})
+const modalSubscribe = document.querySelector('.modalSubscribePalestra')
+
+const abrirModalSubscribe = (data) => {  
+    
+    setDataModal(data)  
+    modalSubscribe.classList.add("active")
+
+}
+const setDataModal = (data) => {
+
+    const img_card_palestra = document.getElementById("img_card_palestraModal")
+    const titleCardPalestraSubscribe = document.getElementById("titleCardPalestraSubscribe")
+    const subTitleCardPalestraSubscribe = document.getElementById("subTitleCardPalestraSubscribe")
+    const img_palestranteSubscribe = document.getElementById("img_palestranteSubscribe")
+    const titleHeaderSubscribe = document.getElementById("titleHeaderSubscribe")
+    const subtitleHeaderSubscribe = document.getElementById("subtitleHeaderSubscribe")
+    const aboutPalestraSubscribe = document.getElementById("aboutPalestraSubscribe")
+    const nomePalestranteSubscribe = document.getElementById("nomePalestranteSubscribe") 
+
+    img_card_palestra.src = data.srcImagemPalestra
+    titleCardPalestraSubscribe.innerText = data.titlePalestra
+    subTitleCardPalestraSubscribe.innerText = data.subtitlePalestra
+    img_palestranteSubscribe.src = data.palestrante.fotoSrc
+    titleHeaderSubscribe.innerText = data.titlePalestra
+    subtitleHeaderSubscribe.innerText = data.subtitlePalestra
+    aboutPalestraSubscribe.innerText =  data.descPalestra
+    nomePalestranteSubscribe.innerText = data.palestrante.nome 
+
+}
+
+
+const closeButtonSubscribe = document.getElementById("closeButtonSubscribe") 
+closeButtonSubscribe.addEventListener('click',(e) => {
+
+    closeModalSubscribe()
+
+})
+const closeModalSubscribe = () => { 
+    modalSubscribe.classList.remove("active")
+}
+
+
+
+const oqItemAbout = document.querySelector("#oqItemAbout")
+const paraQuemItemAbout = document.querySelector("#paraQuemItemAbout")
+const pqItemAbout = document.querySelector("#pqItemAbout")
+const descAboutHub = document.getElementById("descAboutHub")
+
+
+oqItemAbout.addEventListener("click",() => { 
+    oqItemAbout.classList.add("active") 
+    paraQuemItemAbout.classList.remove("active")
+    pqItemAbout.classList.remove("active") 
+    descAboutHub.innerText = textsAbout[0] 
+})
+paraQuemItemAbout.addEventListener("click",() => { 
+    paraQuemItemAbout.classList.add("active")
+    oqItemAbout.classList.remove("active") 
+    pqItemAbout.classList.remove("active")   
+    
+    descAboutHub.innerText = textsAbout[1] 
+})
+pqItemAbout.addEventListener("click",() => { 
+    pqItemAbout.classList.add("active")
+    oqItemAbout.classList.remove("active") 
+    paraQuemItemAbout.classList.remove("active") 
+    descAboutHub.innerText = textsAbout[2]
+})
+
+const buttonAreaHamburguerNavBar = document.getElementById("buttonAreaHamburguerNavBar")
+const yParte = document.getElementById("yParte")
+
+buttonAreaHamburguerNavBar.addEventListener("click",() => { 
+    yParte.classList.toggle("active") 
+}) 
